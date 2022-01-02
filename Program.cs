775b5eed760d8448670ff494be7398e0ea50a1ba
Kacper <kacpermichalski132@gmail.com>
2022-01-02 18:36:00 +0100

    Creating main launch program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ProgrammingChallenges;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hi!");
+        Console.WriteLine("This is a program which aggregates all the other mini programs that I wrote for ProgrammingChallanges.");
+        Console.WriteLine("Here are the ones that I did already:");
+        Console.WriteLine();
+        Console.WriteLine(" 01. Guess The Number");
+        Console.WriteLine();
+        Console.WriteLine("Type number to select mini program.");
+        Console.Write("Number: ");
+        string? input = Console.ReadLine();
+
+        switch(int.Parse(input))
+        {
+            case 1:
+                // Odpal
+                var Program = new GuessTheNumber();
+                Program.Start();
+                break;
+            default:
+                Console.WriteLine("Brak programu o takim numerze.");
+                break;
+        }
+    }
+}
