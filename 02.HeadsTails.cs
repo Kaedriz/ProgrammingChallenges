7dde869a09186b2dbf250a40372fdd853b034e6f
Kacper <kacpermichalski132@gmail.com>
2022-01-09 14:59:07 +0100

    Initial version
    CH#02

--- 02.HeadsTails.cs
+++ 02.HeadsTails.cs
@@ -14,6 +14,38 @@ class HeadsTails: IChallange
     }
     public void Start()
     {
-        Console.WriteLine("");
+        // Creating random number
+        bool Heads = Convert.ToBoolean(Random());
+
+        Console.WriteLine("Guess if this will be 'Heads' or 'Tails'.");
+        Console.Write("Your guess: ");
+        string? input = Console.ReadLine();
+        switch(input)
+        {
+            case "Heads":
+                if(Heads)
+                {
+                    Console.WriteLine("Congratulations! You guessed right.");
+                } else
+                {
+                    Console.WriteLine("Sorry! You didn't guess right.");
+                }
+                break;
+            case "Tails":
+                if (Heads)
+                {
+                    Console.WriteLine("Sorry! You didn't guess right.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed right.");
+                }
+                break;
+            default:
+                Console.WriteLine("Incompatible answer!");
+                break;
+        }
+
+
     }
 }
