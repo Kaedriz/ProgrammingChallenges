9741d5bf722a38aba0c747372fd50c301980e929
Kacper <kacpermichalski132@gmail.com>
2022-01-09 13:03:33 +0100

    CH#02 - Initialization

---
+++ 02.HeadsTails.cs
@@ -0,0 +1,15 @@
+﻿namespace ProgrammingChallenges;
+
+class HeadsTails
+{
+    private static int Random()
+    {
+        // Generating random number
+        Random Random = new Random();
+        return Random.Next(1, 101);
+    }
+    public void Start()
+    {
+        Console.WriteLine("");
+    }
+}
